d8130acb7cbaad2cff7fc63cdd2d276c4ac0295e
Sylphy0052 <sylphy0052@gmail.com>
2019-06-24 00:10:19 +0900

    [update] jar file

--- prac1/src/Main.java
+++ prac1/src/Main.java
@@ -4,9 +4,9 @@ import parser.MyParserFactory;
 public class Main {
     public static void main(String[] args) {
         MyParserFactory mrf = new MyParserFactory();
-//        System.out.println("---XML---");
-//        MyFileParser xmlReader = mrf.getParser("./src/files/banana.xml");
-//        xmlReader.show();
+        System.out.println("---XML---");
+        MyFileParser xmlReader = mrf.getParser("./src/files/banana.xml");
+        xmlReader.show();
         System.out.println("---JSON---");
         MyFileParser jsonReader = mrf.getParser("./src/files/apple.json");
         jsonReader.show();
